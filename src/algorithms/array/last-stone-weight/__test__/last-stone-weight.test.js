c41d483bbd64bf014e10fa2ccecde009baf2cbf9
Vijay Ganesh H <vijay.g1@samsung.com>
2020-05-17 15:13:47 +0530

    PROB-2 Re-arrange strings so that no two chars are adjacent

--- src/algorithms/array/last-stone-weight/__test__/last-stone-weight.test.js
+++ src/algorithms/array/last-stone-weight/__test__/last-stone-weight.test.js
@@ -2,7 +2,7 @@ const lastStoneWeight = require('../last-stone-weight'),
     expect = require('chai').expect;
 
 
-describe.only('Last stone weight', () => {
+describe('Last stone weight', () => {
   it('Last stone weight ', () => {
     
     let res = lastStoneWeight([2,7,4,1,8,1]);
